22026f0f76ebb26ec5e1ed3bafdd058af2715e66
Josue Ramirez <josueramirez0110@gmail.com>
2022-03-02 17:20:14 -0600

    The ifs were changed to ternary operators and the for was changed to the Filter method

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { pokemon } = require('./pokedex.json')
+const { pokemon } = require("./pokedex.json");
 
 // Routes
-app.get('/', (req, res, next) => {
-    res.status(200);
-    res.send('Bienvenido a la Pokedex');
+app.get("/", (req, res, next) => {
+  return res.status(200).send("Bienvenido a la Pokedex");
 });
 
-app.get('/pokemon/all', (req, res, next) => {
-    res.status(200);
-    res.send(pokemon)
+app.get("/pokemon", (req, res, next) => {
+  return res.status(200).send(pokemon);
 });
 
-app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
-    const id = req.params.id - 1;
-    if(id >= 0 && id <= 150) {
-        res.status(200);
-        res.send(pokemon[id]);
-    } else {
-        res.status(404);
-        res.send('Pokemon no encontrado');
-    }
+app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
+  const id = req.params.id - 1;
+  (id >= 0 && id <= 150) ? 
+    res.status(200).send(pokemon[id]) : 
+    res.status(404).send("Pokemon no encontrado");
 });
 
-app.get('/pokemon/:name', (req, res, next) => {
-    const name = req.params.name;
-    for(i = 0; i < pokemon.length; i++) {
-        if(pokemon[i].name == name) {
-            res.status(200);
-            return res.send(pokemon[i]);
-        }
-    } 
-    res.status(404);
-    res.send('Pokemon no encontrado');
+app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) => {
+  const name = req.params.name;
+
+  const pokemonFilter = pokemon.filter((p) => {
+    return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+  });
+
+  (pokemonFilter.length > 0) ? 
+    res.status(200).send(pokemonFilter) : 
+    res.status(404).send("Pokemon no encontrado");
 });
 
 // Server
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running...')
+  console.log("Server is running...");
 });
