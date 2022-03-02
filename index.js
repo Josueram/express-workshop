b9240c93cd42e67c84eafbc7a93ffb988351f865
Josue Ramirez <josueramirez0110@gmail.com>
2022-03-02 17:43:37 -0600

    Added a post route

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const { pokemon } = require("./pokedex.json");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Routes
 app.get("/", (req, res, next) => {
   return res.status(200).send("Bienvenido a la Pokedex");
+});
+
+app.post("/pokemon", (req, res, next) => {
+  return res.status(200).send(req.body.name);
 });
 
 app.get("/pokemon", (req, res, next) => {
@@ -13,21 +20,21 @@ app.get("/pokemon", (req, res, next) => {
 
 app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
   const id = req.params.id - 1;
-  (id >= 0 && id <= 150) ? 
-    res.status(200).send(pokemon[id]) : 
-    res.status(404).send("Pokemon no encontrado");
+  id >= 0 && id <= 150
+    ? res.status(200).send(pokemon[id])
+    : res.status(404).send("Pokemon no encontrado");
 });
 
 app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) => {
   const name = req.params.name;
 
   const pokemonFilter = pokemon.filter((p) => {
-    return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+    return p.name.toUpperCase() == name.toUpperCase() ? p : null;
   });
 
-  (pokemonFilter.length > 0) ? 
-    res.status(200).send(pokemonFilter) : 
-    res.status(404).send("Pokemon no encontrado");
+  pokemonFilter.length > 0
+    ? res.status(200).send(pokemonFilter)
+    : res.status(404).send("Pokemon no encontrado");
 });
 
 // Server
