1dc219b926cf7411830aaa805de2dc4a018799eb
Josue Ramirez <josueramirez0110@gmail.com>
2022-03-09 16:33:16 -0600

    Added a standard for error handling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const morgan = require("morgan");
 const express = require("express");
 const app = express();
-const pokemon = require("./routes/pokemon")
+const pokemon = require("./routes/pokemon");
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -9,10 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get("/", (req, res, next) => {
-  return res.status(200).send("Bienvenido a la Pokedex");
+  return res.status(200).json({ code: 1, message: "Bienvenido a la Pokedex" });
 });
 
 app.use("/pokemon", pokemon);
+
+// error handling
+app.use((req, res, next) => {
+  return res.status(404).json({ code: 400, message: "URL no encontrada" });
+});
 
 // Server
 app.listen(process.env.PORT || 3000, () => {
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,24 +8,28 @@ pokemon.post("/", (req, res, next) => {
 
 pokemon.get("/", async (req, res, next) => {
   const pokedex = await db.query("SELECT * FROM pokemon");
-  return res.status(200).json(pokedex);
+  return res.status(200).json({ code: 1, message: pokedex });
 });
 
 pokemon.get("/:id([0-9]{1,3})", async (req, res, next) => {
   const id = req.params.id;
-  const pokemonId = await db.query(`SELECT * FROM pokemon WHERE pok_id = ${id}`);
-  id >= 0 && id <= 150
-    ? res.status(200).json(pokemonId)
-    : res.status(404).json("Pokemon no encontrado");
+  const pokemonId = await db.query(
+    `SELECT * FROM pokemon WHERE pok_id = ${id}`
+  );
+  id >= 0 && id <= 722
+    ? res.status(200).json({ code: 1, message: pokemonId })
+    : res.status(404).json({ code: 404, message: "Pokemon no encontrado" });
 });
 
 pokemon.get("/:name([A-Za-z]+)", async (req, res, next) => {
   const name = req.params.name;
-  const pokemonName = await db.query(`SELECT * FROM pokemon WHERE pok_name = "${name}"`);
+  const pokemonName = await db.query(
+    `SELECT * FROM pokemon WHERE pok_name = "${name}"`
+  );
 
   pokemonName.length > 0
-    ? res.status(200).json(pokemonName)
-    : res.status(404).json("Pokemon no encontrado");
+    ? res.status(200).json({ code: 1, message: pokemonName })
+    : res.status(404).json({ code: 404, message: "Pokemon no encontrado" });
 });
 
 module.exports = pokemon;
